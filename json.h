17da42374b95c6612af64ee71df5e7fe87e956b0
Philippe Gaultier <philippe.gaultier@gi-de.com>
2023-12-16 09:26:58 +0100

    test

--- json.h
+++ json.h
@@ -289,6 +289,15 @@ static void test_json_parse(void) {
     pg_assert(j == NULL);
   }
   {
+    const Str in = str_from_c("null");
+    u8 mem[256] = {0};
+    Arena arena = arena_from_mem(mem, sizeof(mem));
+    Read_cursor cursor = {.s = in};
+
+    const Json *const j = json_parse(&cursor, &arena);
+    pg_assert(j == NULL);
+  }
+  {
     const Str in = str_from_c("123");
     u8 mem[256] = {0};
     Arena arena = arena_from_mem(mem, sizeof(mem));
